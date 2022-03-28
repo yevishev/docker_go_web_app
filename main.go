64786ad760a4113d56b6b64737a135964322bb93
yevishev <yevishev.m@gmail.com>
2022-03-28 15:04:22 +0300

    added struct for headers

--- main.go
+++ main.go
@@ -5,37 +5,41 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"io/ioutil"
-	"math/rand"
 	"net/http"
-	"strconv"
 )
 
-func main() {
-	router := mux.NewRouter().StrictSlash(true)
-
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<h1>This is the homepage. Try /hello and /hello/Sammy\n</h1>")
-	})
-
-	router.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<h1>Hello from Docker!\n</h1>")
-	})
-
-	router.HandleFunc("/hello/{name}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		title := vars["name"]
-
-		fmt.Fprintf(w, "<h1>Hey hey, %s!\n</h1>", title)
-	})
-
-	router.HandleFunc("/excel", excelPost).Methods("POST")
-	router.HandleFunc("/excel", excelGet).Methods("GET")
-
-
-	http.ListenAndServe(":80", router)
+type Header struct {
+	Coordinate string `json:"c"`
+	Text string `json:"v"`
+	Width int `json:"w"`
 }
 
-func excelGet(w http.ResponseWriter, r *http.Request) {
+type Json struct {
+	Headers []Header `json:"h"`
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	/* Homepage */
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<h1>This is the homepage. Try /excel and set .json body\n</h1>")
+	})
+
+	/* Post request test */
+	router.HandleFunc("/excelp", excelPostTest).Methods("POST")
+
+	/* Get request test */
+	router.HandleFunc("/excelg", excelGetTest).Methods("GET")
+
+	/* Get request test */
+	router.HandleFunc("/excel", excelPost).Methods("POST")
+
+	/* Specifying ports */
+	http.ListenAndServe(":8002", router)
+}
+
+func excelGetTest(w http.ResponseWriter, r *http.Request) {
 	var res = make(map[string]string)
 	var status = http.StatusOK
 
@@ -57,12 +61,12 @@ func excelGet(w http.ResponseWriter, r *http.Request) {
 	w.Write(response)
 }
 
-func excelPost(w http.ResponseWriter, r *http.Request) {
-	//request handling
+func excelPostTest(w http.ResponseWriter, r *http.Request) {
 	var req map[string]interface{}
 	body, _ := ioutil.ReadAll(r.Body)
 	err := json.Unmarshal(body, &req)
 	if err != nil {
+		fmt.Println(err)
 		return
 	}
 	id := req["id"].(string)
@@ -70,7 +74,7 @@ func excelPost(w http.ResponseWriter, r *http.Request) {
 
 	//response
 	var res = map[string]string{
-		"id" : strconv.Itoa(rand.Intn(10000000)),
+		"id" : id,
 		"title" : title,
 	}
 	fmt.Println("Post id: " ,id)
@@ -78,4 +82,17 @@ func excelPost(w http.ResponseWriter, r *http.Request) {
 	response, _ := json.Marshal(res)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(response)
+}
+
+func excelPost(w http.ResponseWriter, r *http.Request) {
+	body, _ := ioutil.ReadAll(r.Body)
+	j := Json{}
+	err := json.Unmarshal(body, &j)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, item := range j.Headers {
+		fmt.Printf("%v\n", item.Coordinate)
+	}
 }
